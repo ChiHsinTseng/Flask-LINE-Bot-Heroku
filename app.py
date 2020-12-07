c003a5e10a9f802db3f3a504688726851990fe34
May Lin <71620342+yumei86@users.noreply.github.com>
2020-12-07 21:51:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,44 @@
-import os
-from datetime import datetime
+from flask import Flask, request, abort
 
-from flask import Flask, abort, request
-
-# https://github.com/line/line-bot-sdk-python
-
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
+line_bot_api = LineBotApi('YOUR_Channel_access_token')
+handler = WebhookHandler('YOUR_Channel_secret')
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
 
-
+@app.route("/", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-    if request.method == 'POST':
-        signature = request.META['HTTP_X_LINE_SIGNATURE']
-        body = request.body.decode('utf-8')
- 
-        try:
-            events = parser.parse(body, signature)  # 傳入的事件
-        except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
-            return HttpResponseBadRequest()
- 
-        for event in events:
-            if isinstance(event, MessageEvent):  # 如果有訊息事件
- 
-                if event.message.text == "拉麵推薦":
- 
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text='Buttons template',
-                            template=ButtonsTemplate(
-                                title='Menu',
-                                text='請選擇地區',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label='北部',
-                                        text='北部'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='中部',
-                                        text='中部'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='南部',
-                                        text='南部'
-                                    )
-                                    MessageTemplateAction(
-                                        label='東部',
-                                        text='東部'
-                                    )
-                                ]
-                            )
-                        )
-                    )
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+       abort(400)
 
-        
-             else:
-                def handle_message(event):
-                    get_message = event.message.text
+    return 'OK'
 
-                    # Send To Line
-                    reply = TextSendMessage(text=f"{get_message}")
-                    line_bot_api.reply_message(event.reply_token, reply)
 
- 
-        return HttpResponse()
-    else:
-        return HttpResponseBadRequest()
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    #print(type(msg))
+    msg = msg.encode('utf-8')  
+    if event.message.text == "文字":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
