abc979777f6513a01570b7cb5dfef78c3bc3d2b0
May Lin <71620342+yumei86@users.noreply.github.com>
2020-12-08 12:40:01 +0800

    test

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def handle_message(event):
 
     
     if event.message.text == "拉麵推薦":       
-        buttons_template = TemplateSendMessage(
+        buttons_template1 = TemplateSendMessage(
         alt_text='你想吃哪裡的拉麵？',
         template=ButtonsTemplate(
             title='你想吃哪裡的拉麵？',
@@ -70,11 +70,43 @@ def handle_message(event):
             ]
         )
     )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
+        line_bot_api.reply_message(event.reply_token, buttons_template1)
  
 
 
     elif event.message.text == "錯誤回報":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "請點選以下連結：https://reurl.cc/14RmVW"))
- 
-   
+
+        
+        
+    elif event.message.text == "北部":       
+        buttons_template2 = TemplateSendMessage(
+        alt_text='你在哪個縣市？',
+        template=ButtonsTemplate(
+            title='你在哪個縣市？',
+            text='讓我知道你在哪個縣市會加速我找到適合的店喔！',
+            thumbnail_image_url='https://1.bp.blogspot.com/-ZWe-zRa3stE/XlTGFl7NksI/AAAAAAAAvmY/fR_RrfO6GAYdQSirnCC-bqvelI7LkUnYwCEwYBhgL/s1600/ACA5ABA1-3DDE-4042-8205-2572D5AFD0B5.JPG',
+            actions=[
+                MessageTemplateAction(
+                    typ = bubble
+                    label='基隆市',
+                    text='基隆市'
+                ),
+                 MessageTemplateAction(
+                    label='台北市',
+                    text='台北市'
+                ),
+                MessageTemplateAction(
+                    label='桃園市',
+                    text='桃園市'
+                ),
+                MessageTemplateAction(
+                    label='新竹縣',
+                    text='新竹縣'
+                ),  
+
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template2) 
+
