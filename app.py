21640b7d171cc01f93191e7d62f52a7ef2f1573f
May Lin <71620342+yumei86@users.noreply.github.com>
2020-12-08 10:25:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def handle_message(event):
     
     if event.message.text == "拉麵推薦":       
         buttons_template = TemplateSendMessage(
-        alt_text='Buttons Template',
+        alt_text='你想吃哪裡的拉麵？',
         template=ButtonsTemplate(
             title='你想吃哪裡的拉麵？',
             text='最好選近的不然你會餓死',
@@ -73,4 +73,34 @@ def handle_message(event):
     
     elif event.message.text == "錯誤回報":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "請點選以下連結：https://reurl.cc/14RmVW"))
+ 
+    elif event.message.text == "北部":       
+        buttons_template = TemplateSendMessage(
+        alt_text='Buttons Template',
+        template=ButtonsTemplate(
+            title='你想知道哪個縣市？',
+            text='test',
+            thumbnail_image_url='https://1.bp.blogspot.com/-ZWe-zRa3stE/XlTGFl7NksI/AAAAAAAAvmY/fR_RrfO6GAYdQSirnCC-bqvelI7LkUnYwCEwYBhgL/s1600/ACA5ABA1-3DDE-4042-8205-2572D5AFD0B5.JPG',
+            actions=[
+                MessageTemplateAction(
+                    label='北部',
+                    text='北部'
+                ),
+                 MessageTemplateAction(
+                    label='中部',
+                    text='中部'
+                ),
+                MessageTemplateAction(
+                    label='南部',
+                    text='南部'
+                ),
+                MessageTemplateAction(
+                    label='東部',
+                    text='東部'
+                ),  
+
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template)
    
