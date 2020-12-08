67fa2941620ee342a6599f5934049bb26dcb124c
May Lin <71620342+yumei86@users.noreply.github.com>
2020-12-08 16:07:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -413,3 +413,137 @@ def handle_message(event):
         )
         
         line_bot_api.reply_message(event.reply_token,flex_message2) 
+        
+    elif event.message.text == "南部":
+        flex_message3 = FlexSendMessage(
+        alt_text='南部的縣市',
+        contents={
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "你在哪個縣市？",
+        "weight": "bold",
+        "size": "xl"
+      },
+      {
+        "type": "text",
+        "text": "請幫助我選擇縣市讓你更快得到推薦喔！",
+        "size": "xs",
+        "margin": "lg",
+        "color": "#888888"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "margin": "lg",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "text": "嘉義市",
+              "label": "嘉義市"
+            },
+            "style": "secondary",
+            "height": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "嘉義縣",
+              "text": "嘉義縣"
+            },
+            "style": "secondary",
+            "height": "sm",
+            "margin": "xxl"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "margin": "md",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "text": "台南市",
+              "label": "台南市"
+            },
+            "style": "secondary",
+            "height": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "高雄市",
+              "text": "高雄市"
+            },
+            "style": "secondary",
+            "height": "sm",
+            "margin": "xxl"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "text": "屏東縣",
+              "label": "屏東縣"
+            },
+            "style": "secondary",
+            "height": "sm"
+          },
+          {
+            "type": "text",
+            "text": " ",
+            "margin": "xxl"
+          }
+        ],
+        "margin": "md"
+      }
+    ]
+  }
+}
+        )
+        
+        line_bot_api.reply_message(event.reply_token,flex_message3) 
+       
+    
+ '''
+    elif event.message.text == "南部":
+        flex_message3 = FlexSendMessage(
+        alt_text='南部的縣市',
+        contents=
+
+        )
+        
+        line_bot_api.reply_message(event.reply_token,flex_message3) 
+ '''
+
